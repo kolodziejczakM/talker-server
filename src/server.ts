9cc9cd91af00fa67a844c0b0f2f4e4430839f451
kolodziejczakM <kolodziejczak.mn@gmail.com>
2018-10-28 18:19:12 +0100

    Added basic JWT config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,45 @@
-import express from 'express';
+import express, { Request } from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema/schema';
 import socketIO from 'socket.io';
+import jwt from 'express-jwt';
+import config from '../config.json';
+// import jsonwebtoken from 'jsonwebtoken';
+
+// TODO: move to another file
+interface RequestNotAuthenticated extends Request {
+    user: Object;
+}
 
 const app = express();
 const server = app.listen(4002);
 const io = socketIO.listen(server);
+const auth = jwt({
+    secret: config.jwtSecret,
+    credentialsRequired: false, // Users should to be able to at least signup and login first.
+    getToken: req => {
+        if (
+            req.headers.authorization &&
+            req.headers.authorization.split(' ')[0] === 'Bearer'
+        ) {
+            return req.headers.authorization.split(' ')[1];
+        }
 
-app.use('/graphql', graphqlHTTP({ schema }));
+        if (req.query && req.query.token) {
+            return req.query.token;
+        }
+        return null;
+    }
+});
+
+app.use(
+    '/graphql',
+    auth,
+    graphqlHTTP({
+        schema,
+        graphiql: false // Boolean(process.env.development)
+    })
+);
 
 app.get('/', (req, res) => {
     res.send('OK');
